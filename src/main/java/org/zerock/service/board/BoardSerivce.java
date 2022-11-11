9c835b38ac65c51d4c779f51cae45b67f1d5ec24
tlswnsgk <110441845+tlswnsgk@users.noreply.github.com>
2022-11-11 10:38:19 +0900

    221111 수정전

--- src/main/java/org/zerock/service/board/BoardSerivce.java
+++ src/main/java/org/zerock/service/board/BoardSerivce.java
@@ -16,7 +16,6 @@ import org.zerock.mapper.board.ReplyMapper;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.DeletedObject;
 import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
@@ -26,74 +25,78 @@ public class BoardSerivce {
 
 	@Autowired
 	private BoardMapper boardMapper;
-	
+
 	@Autowired
 	private ReplyMapper replyMapper;
-	
+
 	@Autowired
 	private S3Client s3Client;
-	
+
 	@Value("${aws.s3.bucket}")
 	private String bucketName;
-	
+
 	public int register(BoardDto board, MultipartFile[] files) {
 		// db에 게시물 정보 저장
 		int cnt = boardMapper.insert(board);
-		
+
 		for (MultipartFile file : files) {
 			if (file != null && file.getSize() > 0) {
 				// db에 파일 정보 저장
 				boardMapper.insertFile(board.getId(), file.getOriginalFilename());
-				
-				try {
-					
-				// 	S3에 파일 저장
-				// 키 생성
-				String key = "prj1/board/"+board.getId() + "/"+file.getOriginalFilename();
-				
-				// putObjectRequest
-				PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-														.bucket(bucketName)
-														.key(key)
-														.acl(ObjectCannedACL.PUBLIC_READ)
-														.build();
-				
-				//requestBody
-				RequestBody requestBody = RequestBody.fromInputStream(file.getInputStream(), file.getSize());
-				
-				// object(파일) 올리기
-				s3Client.putObject(putObjectRequest, requestBody);
-				
-				}catch(Exception e) {
-					e.printStackTrace();
-					throw new RuntimeException(e);
-				}
+
+				uploadFile(board.getId(), file);
 			}
 		}
-		
+
 		return cnt;
+	}
+
+	private void uploadFile(int id, MultipartFile file) {
+		try {
+
+			// S3에 파일 저장
+			// 키 생성
+			String key = "prj1/board/" + id + "/" + file.getOriginalFilename();
+
+			// putObjectRequest
+			PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+					.bucket(bucketName)
+					.key(key)
+					.acl(ObjectCannedACL.PUBLIC_READ)
+					.build();
+
+			// requestBody
+			RequestBody requestBody = RequestBody.fromInputStream(file.getInputStream(), file.getSize());
+
+			// object(파일) 올리기
+			s3Client.putObject(putObjectRequest, requestBody);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 	}
 
 	public List<BoardDto> listBoard(int page, String type, String keyword, PageInfo pageInfo) {
 		int records = 10;
 		int offset = (page - 1) * records;
-		
+
 		int countAll = boardMapper.countAll(type, "%" + keyword + "%"); // SELECT Count(*) FROM Board
 		int lastPage = (countAll - 1) / records + 1;
-		
+
 		int leftPageNumber = (page - 1) / 10 * 10 + 1;
 		int rightPageNumber = leftPageNumber + 9;
 		rightPageNumber = Math.min(rightPageNumber, lastPage);
-		
+
 		// 이전버튼 유무
 		boolean hasPrevButton = page > 10;
 		// 다음버튼 유무
 		boolean hasNextButton = page <= ((lastPage - 1) / 10 * 10);
-		
+
 		// 이전버튼 눌렀을 때 가는 페이지 번호
 		int jumpPrevPageNumber = (page - 1) / 10 * 10 - 9;
-		int jumpNextPageNumber = (page - 1) / 10 * 10 + 11; 
-		
+		int jumpNextPageNumber = (page - 1) / 10 * 10 + 11;
+
 		pageInfo.setHasPrevButton(hasPrevButton);
 		pageInfo.setHasNextButton(hasNextButton);
 		pageInfo.setJumpPrevPageNumber(jumpPrevPageNumber);
@@ -102,7 +105,7 @@ public class BoardSerivce {
 		pageInfo.setLeftPageNumber(leftPageNumber);
 		pageInfo.setRightPageNumber(rightPageNumber);
 		pageInfo.setLastPageNumber(lastPage);
-		
+
 		return boardMapper.list(offset, records, type, "%" + keyword + "%");
 	}
 
@@ -113,85 +116,68 @@ public class BoardSerivce {
 
 	public int update(BoardDto board, MultipartFile[] addFiles, List<String> removeFiles) {
 		int boardId = board.getId();
-		
-		// removeFiles 에 있는 파일명으로 
-		
+
+		// removeFiles 에 있는 파일명으로
+
 		if (removeFiles != null) {
 			for (String fileName : removeFiles) {
 				// 1. File 테이블에서 record 지우기
 				boardMapper.deleteFileByBoardIdAndFileName(boardId, fileName);
-				// 2. 저장소에 실제 파일 지우기
-				String path = "C:\\Users\\user\\Desktop\\study\\upload\\prj1\\board\\" + boardId + "\\" + fileName;
-				File file = new File(path);
-				
-				file.delete();
+				// 2. s3저장소에 실제 파일 지우기
+				deleteFile(boardId, fileName);
 			}
 		}
-		
-		
+
 		for (MultipartFile file : addFiles) {
 			if (file != null && file.getSize() > 0) {
 				String name = file.getOriginalFilename();
 				// File table에 해당파일명 지우기
 				boardMapper.deleteFileByBoardIdAndFileName(boardId, name);
-				
+
 				// File table에 파일명 추가
 				boardMapper.insertFile(boardId, name);
-				
-				// 저장소에 실제 파일 추가
-				File folder = new File("C:\\Users\\user\\Desktop\\study\\upload\\prj1\\board\\" + board.getId());
-				folder.mkdirs();
-				
-				File dest = new File(folder, name);
-				
-				try {
-					file.transferTo(dest);
-				} catch (Exception e) {
-					// @Transactional은 RuntimeException에서만 rollback 됨
-					e.printStackTrace();
-					throw new RuntimeException(e);
-				}
+
+				// s3저장소에 실제 파일(object) 추가
+				uploadFile(boardId, file);
 			}
-			
+
 		}
-		
-		
-		return boardMapper.update(board);	
+
+		return boardMapper.update(board);
 	}
 
 	public int remove(int id) {
 		BoardDto board = boardMapper.select(id);
-		
+
 		List<String> fileNames = board.getFileName();
-		if(fileNames != null) {
-			for(String fileName : fileNames) {
-				
-			// s3저장소의 파일 지우기
-			String key = "prj1/board/" + id+"/" + fileName;
-			DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
-					.bucket(bucketName)
-					.key(key)
-					.build();
-			s3Client.deleteObject(deleteObjectRequest);
-			
+		if (fileNames != null) {
+
+			for (String fileName : fileNames) {
+				deleteFile(id, fileName);
 			}
 		}
+
 		// db 파일 records 지우기
 		boardMapper.deleteFileByBoardId(id);
-		
-		
+
 		// 게시물의 댓글들 지우기
 		replyMapper.deleteByBoardId(id);
-		
+
 //		int a = 3 / 0; // runtime exception
-		
+
 		// 게시물 지우기
 		return boardMapper.delete(id);
 	}
-	
+
+	private void deleteFile(int id, String fileName) {
+		// s3저장소의 파일 지우기
+		String key = "prj1/board/" + id + "/" + fileName;
+		DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+				.bucket(bucketName)
+				.key(key)
+				.build();
+
+		s3Client.deleteObject(deleteObjectRequest);
+	}
+
 }
-
-
-
-
-
--- src/main/webapp/WEB-INF/tags/navBar.tag
+++ src/main/webapp/WEB-INF/tags/navBar.tag
@@ -3,13 +3,17 @@
 <%@ attribute name="active" %>
 
 <style>
-	#searchTypeSelect{
-	width :auto;
-	}
+#searchTypeSelect {
+	width: auto;
+}
 </style>
+
 
 <c:url value="/board/list" var="listLink" />
 <c:url value="/board/register" var="registerLink" />
+<c:url value="/member/signup" var="signupLink" />
+<c:url value="/member/list" var="memberListLink" />
+
 
 <nav class="navbar navbar-expand-md bg-light">
   <div class="container-md">
@@ -18,23 +22,30 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav me-auto">
         <li class="nav-item">
           <a class="nav-link ${active eq 'list' ? 'active' : '' }" href="${listLink }">목록</a>
         </li>
         <li class="nav-item">
           <a class="nav-link ${active eq 'register' ? 'active' : '' }" href="${registerLink }">작성</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link ${active eq 'memberList' ? 'active' : '' }" href="${memberListLink }">회원목록</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link ${active eq 'signup' ? 'active' : '' }" href="${signupLink }">회원가입</a>
+        </li>
         
       </ul>
       <form action="${listLink }" class="d-flex" role="search">
       
-      <select name="t" id="searchTypeSelect" class="form-select">
-      	<option value="all">전체</option>
-      	<option value="title" ${param.t=='title'?'selected':'' }>제목</option>
-      	<option value="content"${param.t=='content'?'selected':'' } >본문</option>
-      	<option value="writer"${param.t=='writer'?'selected':'' } >작성자</option>
-      </select>
+      	<select name="t" id="searchTypeSelect" class="form-select">
+      		<option value="all">전체</option>
+      		<option value="title" ${param.t == 'title' ? 'selected' : '' }>제목</option>
+      		<option value="content" ${param.t == 'content' ? 'selected' : '' }>본문</option>
+      		<option value="writer" ${param.t == 'writer' ? 'selected' : '' }>작성자</option>
+      	</select>
       
         <input value="${param.q }" class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="q">
         <button class="btn btn-outline-success" type="submit">
